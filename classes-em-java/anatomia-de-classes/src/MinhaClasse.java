c49ede44fe73db50ed206ff38a527b226fc17883
laurasbessa <laurasbessa@gmail.com>
2024-05-03 17:31:25 -0300

    variaveis e metodos

--- classes-em-java/anatomia-de-classes/src/MinhaClasse.java
+++ classes-em-java/anatomia-de-classes/src/MinhaClasse.java
@@ -1,6 +1,23 @@
 public class MinhaClasse {
     
 public static void main(String[] args) {
-    System.out.print("Seja bem-vindo!");
+//declaração de variáveis:
+    String primeiroNome = "Laura";
+    String segundoNome = "Bessa";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+    System.out.println(nomeCompleto);
+
 }
+
+//métodos:
+//TipoRetorno NomeObjetivonoInfinitivo Parametro(s)
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return "Resultado do método " +   primeiroNome.concat(" ").concat(segundoNome);
+
+
+
+}
+
 }
